2535002150ee777b43c18c3a1b4ed4ca8e5a6722
SushilDhore <67548707+SushilDhore@users.noreply.github.com>
2022-05-16 20:57:34 +0530

    Update first.tf

--- first.tf
+++ first.tf
@@ -1,8 +1,8 @@
 provider "aws" {
 
 region = "ap-south-1"
-access_key = "AKIA4DC7AYAWI5WXXSUI"
-secret_key = "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" 
+access_key = "AKIA4XXSUI"
+secret_key = "EPlS+RwLGfe5DHkD" 
 
 
 }
